d727dc96504aa58da89785337da7c88d25ffcf44
Amigo538 <amigo538.348@gmail.com>
2025-03-25 22:23:34 +0200

    Add files via upload

---
+++ ConsoleApp37/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp37
+{ 
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            int arraySize = InputIntNum("Array Size : "); int[] array = new int[arraySize];
+            InputIntMasiv(array);            //SortIntMas(array);
+            Array.Sort(array); OutIntMas(array);
+            Console.ReadLine();
+        }
+        static void SortIntMas(int[] omas)
+        {
+            int transfer; for (int i = 0; i < omas.Length - 1; i++)
+            {
+                for (int t = i + 1; t < omas.Length; t++)
+                {
+                    if (omas[i] < omas[t])
+                    {
+                        transfer = omas[i]; omas[i] = omas[t];
+                        omas[t] = transfer;
+                    }
+                }
+            }
+        }
+        static int InputIntNum(string message)
+        {
+            Console.Write(message); return int.Parse(Console.ReadLine());
+        }
+        static void InputIntMasiv(int[] omas)
+        {
+            for (int i = 0; i < omas.Length; i++)
+            {
+                omas[i] = InputIntNum($"omas{i} : ");
+            }
+        }
+        static void OutIntMas(int[] omas)
+        {
+            Console.WriteLine("вывод :"); for (int i = omas.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine($"Omas[{i}] = {omas[i]}");
+            }
+        }
+    }
+}
